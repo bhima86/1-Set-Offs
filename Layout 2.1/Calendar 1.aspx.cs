5aaf340406b630f84a0d98756d5585983762f001
ue <ue@DESKTOP-OVIGJK3>
2023-05-24 11:01:21 +0530

    Add project files.

---
+++ Layout 2.1/Calendar 1.aspx.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class WebForm11 : System.Web.UI.Page
+    {
+        SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-2VT3DAG;Initial Catalog=db1;Integrated Security=True");
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            today();
+            /*
+            if (!IsPostBack)
+            {
+                today();
+            }*/
+            //string UserId = Session["Id"] as string;
+            //Label1.Text= UserId;
+            Session["ID1"] = Session["Id"];
+            string UserId = Session["Id"] as string;
+            Label1.Text= UserId;
+        }
+
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("Create_Abs.aspx");
+        }
+
+        protected void Calendar1_SelectionChanged(object sender, EventArgs e)
+        {
+
+            string SelectDate = Calendar1.SelectedDate.ToString("yyyy-MM-dd");
+            Response.Write(SelectDate);
+
+           
+            con.Open();
+                string query = "SELECT e.FirstName, l.LeaveType FROM Employee e INNER JOIN Leave l ON e.id = l.EMP_ID WHERE  '" + Calendar1.SelectedDate.ToString("yyyy-MM-dd") + "'between l.StartDate AND l.EndDate;";
+                SqlCommand cmd = new SqlCommand(query, con);
+                SqlDataReader sdr = cmd.ExecuteReader();
+                if (sdr.HasRows == true)
+                {
+                    GridView1.DataSource = sdr;
+                    GridView1.DataBind();
+                }
+                else
+                {
+                    //Empty data Table
+                    DataTable dt = new DataTable();
+                    GridView1.DataSource = dt;
+                    GridView1.DataBind();
+                }
+                con.Close();
+            
+                        
+        }    
+        
+        private void today()
+        {
+           
+            string query = "SELECT e.FirstName,l.LeaveType FROM Employee e INNER JOIN Leave l ON e.id = l.EMP_ID where  '" + Calendar1.TodaysDate.ToString("yyyy-MM-dd") + "'between l.StartDate AND l.EndDate";
+            SqlCommand cmd = new SqlCommand(query, con);
+            //SqlCommand cmd = new SqlCommand(query, con);
+            con.Open();
+            SqlDataReader sdr = cmd.ExecuteReader();
+            if(sdr.HasRows == true)
+            {
+                GridView1.DataSource = sdr;
+                GridView1.DataBind();             
+            }
+            else
+            {
+                //Empty data Table
+                DataTable dt = new DataTable();
+                GridView1.DataSource = dt;
+                GridView1.DataBind();
+            }
+            con.Close();
+        }
+
+        protected void Calendar1_DayRender(object sender, DayRenderEventArgs e)
+        {
+            if ( e.Day.IsWeekend)
+            {
+                e.Day.IsSelectable = false;
+                e.Cell.ToolTip = "Chhuti hai bhai..";
+                
+            }
+            if(e.Day.IsOtherMonth )
+            {
+                e.Day.IsSelectable = false;
+            }
+
+            
+
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            string query = "Select Firstname From HolidayTable";
+            SqlCommand cmd2 = new SqlCommand(query, con);
+            con.Open();
+            SqlDataReader sdr2 = cmd2.ExecuteReader();
+            if (sdr2.HasRows == true)
+            {
+                GridView1.DataSource = sdr2;
+                GridView1.DataBind();
+            }
+            // Access the header row of the GridView
+            GridViewRow headerRow = GridView1.HeaderRow;
+            if (headerRow != null)
+            {
+                // Modify the header text
+                headerRow.Cells[0].Text = "Upcoming Holidays";
+                // Replace 0 with the appropriate cell index if you have multiple columns in the header.
+            }
+            con.Close() ;
+        }
+
+
+
+    }
+}
---
+++ Layout 2.1/Create_Abs.aspx.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Reflection.Emit;
+using SetOffs1;
+//using Microsoft.AspNet.FriendlyUrls;
+
+namespace WebApplication1
+{
+     
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        SqlConnection conn= new SqlConnection(@"Data Source=DESKTOP-2VT3DAG;Initial Catalog=db1;Integrated Security=True");
+
+      
+
+        static string b="2";
+        int a = Int16.Parse(b);
+        DateTime from1;
+        DateTime To1;
+
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!IsPostBack)
+            {
+                Calendar1.Visible= false;
+                Calendar2.Visible = false;
+            }
+            // b = Session["ID"]as string;
+           
+
+        }
+     
+
+    protected void Calendar1_Click(object sender, ImageClickEventArgs e)
+        {
+           
+            Calendar2.Visible = false;
+            if (Calendar1.Visible)
+            {
+                Calendar1.Visible= false;
+               
+            }
+            else
+            {
+                Calendar1.Visible= true;
+               
+
+            }
+
+            Calendar1.Attributes.Add("style", "position:absolute");
+
+        }
+
+        protected void Unnamed_Click1(object sender, ImageClickEventArgs e)
+        {
+           
+            Calendar1.Visible = false;
+            if (Calendar2.Visible)
+            {
+                Calendar2.Visible = false;
+            }
+            else
+            {
+                Calendar2.Visible = true;
+            }
+            Calendar2.Attributes.Add("style", "position:absolute");
+            
+        }
+       
+
+        protected void Calendar1_SelectionChanged(object sender, EventArgs e)
+        {
+            from.Text=Calendar1.SelectedDate.ToString("dd/MM/yy");
+            Calendar1.Visible = false;
+             from1 = Convert.ToDateTime(from.Text);
+
+
+        }
+
+        protected void Calendar2_SelectionChanged(object sender, EventArgs e)
+        {
+            To.Text = Calendar2.SelectedDate.ToString("dd/MM/yy");
+            Calendar2.Visible = false;
+            To1 = Convert.ToDateTime(To.Text);
+
+
+
+            DateTime startDate = Calendar1.SelectedDate;
+            DateTime endDate = Calendar2.SelectedDate;
+            TimeSpan difference = endDate - startDate;
+            string m = difference.ToString("dd");
+
+            int n = Int16.Parse(m)+1;
+            Total_Days.Text = n.ToString();
+
+        }
+      
+       
+
+
+        protected void Unnamed_Click2(object sender, EventArgs e)
+        {
+           if(from.Text=="" || To.Text==""|| Drop.SelectedValue== "")
+            {
+               
+                string script = $@"<script type='text/javascript'>alert('{"* Error : Plese fill all information"}');</script>";
+                ClientScript.RegisterStartupScript(this.GetType(), "errorScript", script);
+            }
+            
+
+            //  SqlCommand cmd =conn.CreateCommand();
+            // cmd.CommandType= CommandType.Text;
+
+            //  cmd.CommandText = "insert into ADD values('" + Drop.SelectedValue + "', '" + from + "','" + To + "','" + TextBox1.Text + "' ) ";
+
+
+            //  cmd.ExecuteNonQuery();
+            else {
+
+                string UserId = Session["Id"] as string;
+
+                conn.Open();
+                SqlCommand cmd1 = new SqlCommand("select id from Employee where email='"+UserId+"'",conn);
+                object result = cmd1.ExecuteScalar();
+                if (result != null)
+                {
+                    a = (int)result; // Type casting the result to int and assigning it to employeeId
+                }
+                //else{handle the exception later}
+                Leave l = new Leave();
+                l.LeaveType = Drop.SelectedValue;
+
+                DBConnection cmd = new DBConnection();
+                cmd.AddLeave(l);
+
+
+            //    SqlCommand cmd = new SqlCommand("insert into [Leave]  values('" + a + "','" + Drop.SelectedValue + "', '" + Calendar1.SelectedDate.ToString("yyyy-MM-dd") + "', '" + Calendar2.SelectedDate.ToString("yyyy-MM-dd") +"','" + Total_Days.Text + "','" + TextBox1.Text + "') ", conn);
+            //cmd.ExecuteNonQuery();
+            //conn.Close();
+
+
+                
+                Response.Redirect("Calendar 1.aspx");
+            }
+
+        }
+
+        protected void Calendar1_DayRender(object sender, DayRenderEventArgs e)
+        {
+            if (e.Day.Date < DateTime.Now.Date) // Replace DateTime.Now with your selected value
+            {
+                e.Day.IsSelectable = false;
+                e.Cell.ForeColor = System.Drawing.Color.Gray; // Change the color to gray to indicate the disabled day
+            }
+        }
+
+        protected void Calendar2_DayRender(object sender, DayRenderEventArgs e)
+        {
+            if (e.Day.Date < Calendar1.SelectedDate) // Replace DateTime.Now with your selected value
+            {
+                e.Day.IsSelectable = false;
+                e.Cell.ForeColor = System.Drawing.Color.Gray; // Change the color to gray to indicate the disabled day
+            }
+
+        }
+        
+
+        
+    }
+}
